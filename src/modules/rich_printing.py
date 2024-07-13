a5f47054697913b60226ed491212f86c6f100205
bwrob <bw2002@wp.pl>
2024-07-13 23:53:26 +0200

    rich inspect

--- src/modules/rich_printing.py
+++ src/modules/rich_printing.py
@@ -7,6 +7,18 @@ from rich import print as rich_print
 install()
 
 
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return f"{self.a} {self.b}"
+
+    def do_something(self):
+        return self.a + self.b
+
+
 if __name__ == "__main__":
     rich_print("Hello, World!")
     rich_print("[italic red]Hello[/italic red] World!")
@@ -17,3 +29,9 @@ if __name__ == "__main__":
     ic(__builtins__)
     rich_print(__builtins__)
     inspect(__builtins__)
+
+    example_obj = MyClass(1, 2)
+    inspect(MyClass, all=True)
+    inspect(example_obj, all=True)
+
+    inspect(inspect)
